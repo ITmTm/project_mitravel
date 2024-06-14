810ab5c666e60678dd1bfbdacbda186f5018a3f7
Timur <Stafr95@mail.ru>
2024-06-14 23:13:16 +0500

    Writing a script

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
-const swiperContainer = document.querySelector('.slider-container'),
+const sliderContainer = document.querySelector('.slider-container'),
+	  swiperContainer = document.querySelector('.swiper-container'),
 	  burger = document.querySelector('.burger'),
 	  menu = document.querySelector('.menu'),
 	  close = document.querySelector('.menu__close');
 
-const swiper = new Swiper(swiperContainer, {
+const swiper = new Swiper(sliderContainer, {
 	centeredSlides: true,
 	slidesPerView: 'auto',
 	loop: true,
 	spaceBetween: 105
+});
+
+const secondSwiper = new Swiper(swiperContainer, {
+	centeredSlides: true,
+	slidesPerView: 1,
+	loop: true,
+	spaceBetween: 10,
+	navigation: {
+		nextEl: '.btn-right',
+		prevEl: '.btn-left'
+	}
 });
 
 burger.addEventListener('click', () => {

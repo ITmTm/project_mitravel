37d68df1501db65d9fa8394ed71d3535da2cd2a3
Timur <Stafr95@mail.ru>
2024-06-14 21:13:58 +0500

    Writing a script

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-let swiper = new Swiper('.slider-container', {
+const swiperContainer = document.querySelector('.slider-container'),
+	  burger = document.querySelector('.burger'),
+	  menu = document.querySelector('.menu'),
+	  close = document.querySelector('.menu__close');
+
+const swiper = new Swiper(swiperContainer, {
 	centeredSlides: true,
 	slidesPerView: 'auto',
 	loop: true,
 	spaceBetween: 105
 });
+
+burger.addEventListener('click', () => {
+	menu.classList.add('menu--visible');
+})
+
+close.addEventListener('click', () => {
+	menu.classList.remove('menu--visible');
+})
 
 

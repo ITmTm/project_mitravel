cd6782dc0045594798f7f80f1c646042e3b35883
Timur <Stafr95@mail.ru>
2024-06-15 18:38:41 +0500

    Slider implementation

--- js/main.js
+++ js/main.js
@@ -1,33 +1,65 @@
-const sliderContainer = document.querySelector('.slider-container'),
-	  swiperContainer = document.querySelector('.swiper-container'),
-	  burger = document.querySelector('.burger'),
-	  menu = document.querySelector('.menu'),
-	  close = document.querySelector('.menu__close');
+document.addEventListener('DOMContentLoaded', () => {
+	const sliderContainer = document.querySelector('.slider-container'),
+		swiperContainer = document.querySelector('.swiper-container'),
+		burger = document.querySelector('.burger'),
+		menu = document.querySelector('.menu'),
+		close = document.querySelector('.menu__close'),
+		playButtonsFirst = document.querySelectorAll('.main-slider__play');
 
-const swiper = new Swiper(sliderContainer, {
-	centeredSlides: true,
-	slidesPerView: 'auto',
-	loop: true,
-	spaceBetween: 105
-});
 
-const secondSwiper = new Swiper(swiperContainer, {
-	centeredSlides: true,
-	slidesPerView: 1,
-	loop: true,
-	spaceBetween: 10,
-	navigation: {
-		nextEl: '.btn-right',
-		prevEl: '.btn-left'
-	}
-});
+	let firstSwiper = new Swiper(swiperContainer, {
+		centeredSlides: true,
+		slidesPerView: 1,
+		loop: true,
+		spaceBetween: 10,
+		navigation: {
+			nextEl: '.btn-right',
+			prevEl: '.btn-left'
+		}
+	});
 
-burger.addEventListener('click', () => {
-	menu.classList.add('menu--visible');
+
+	const swiper = new Swiper(sliderContainer, {
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		loop: true,
+		spaceBetween: 105
+	});
+
+
+
+	firstSwiper.on('transitionEnd', function () {
+		let videos = document.querySelectorAll('.first__slider video');
+		videos.forEach((video) => {
+			video.pause();
+			video.currentTime = 0;
+		});
+		playButtonsFirst.forEach((play) => {
+			play.style.display = 'block';
+		})
+	})
+
+	burger.addEventListener('click', () => {
+		menu.classList.add('menu--visible');
+	})
+
+	close.addEventListener('click', () => {
+		menu.classList.remove('menu--visible');
+	})
+
+	playButtonsFirst.forEach((play) => {
+		play.addEventListener('click', (e) => {
+			let video = e.currentTarget.closest('.main-slider__media').querySelector('video');
+			video.play();
+			e.currentTarget.style.display = 'none'
+			setTimeout(() => {
+				video.volume = 0.5;
+			}, 1000)
+		})
+	})
+
+
+
+
 })
-
-close.addEventListener('click', () => {
-	menu.classList.remove('menu--visible');
-})
-
 

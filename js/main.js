5ef8439c4909e41ca321c75b2cfa25b84db05f26
Timur <Stafr95@mail.ru>
2024-06-16 20:36:22 +0500

    Completing the form submission

--- js/main.js
+++ js/main.js
@@ -58,8 +58,47 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	})
 
+	// inputMask
+	let selector = document.querySelectorAll('input[type=tel]');
+
+	let im = new Inputmask('+7 (999) 999-99-99');
+
+	im.mask(selector);
 
 
+	// validate forms
+	let validateForms = function(selector, rules) {
 
+		new window.JustValidate(selector, {
+			rules: rules,
+			submitHandler: function (form, values, ajax) {
+				const formData = new FormData(form);
+
+				const xhr = new XMLHttpRequest();
+
+
+				xhr.onreadystatechange = function () {
+					if (xhr.readyState === 4) {
+
+						if (xhr.status === 200) {
+							console.log('Отправлено!')
+						} else {
+
+						}
+					}
+				}
+
+				// Add any event handlers here...
+				xhr.open('POST', "mail.php", true);
+				xhr.send(formData);
+
+				form.reset();
+			},
+		});
+	}
+
+// validateForms('.mailing__form', { email: { required: true, email: true } }, 'mailing-window', 'materialy s proshloj konferencii');
+	validateForms('.newsletter__form', { email: { required: true, email: true }, tel: { required: true } });
+	validateForms('.subs-form', { email: { required: true, email: true } });
 })
 
